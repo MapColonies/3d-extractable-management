16c051079bd54443cc8abd0675c7e6b883fbc8a5
TULCHINSKI LIRAN <lira.tulchin@gmail.com>
2026-02-04 10:00:56 +0200

    chore: fixed converting timestamp to utc timestamp

---
+++ src/DAL/migrations/1770136414640-AlterAuthorizedAtToTimeStamptz.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class AlterAuthorizedAtToTimeStamptz1770136414640 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "extractable_records" ALTER COLUMN "authorizedAt" TYPE TIMESTAMPTZ`);
+    await queryRunner.query(`ALTER TABLE "audit_log" ALTER COLUMN "authorizedAt" TYPE TIMESTAMPTZ`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "extractable_records" ALTER COLUMN "authorizedAt" TYPE TIMESTAMP`);
+    await queryRunner.query(`ALTER TABLE "audit_log" ALTER COLUMN "authorizedAt" TYPE TIMESTAMP`);
+  }
+}
