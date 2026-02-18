0d2adc37d166af10123f74f37551c6f77ef68358
TULCHINSKI LIRAN <lira.tulchin@gmail.com>
2026-02-18 15:28:39 +0200

    test: added max records config

--- tests/integration/records/records.spec.ts
+++ tests/integration/records/records.spec.ts
@@ -236,8 +236,8 @@ describe('records', function () {
       expect(response.status).toBe(httpStatusCodes.BAD_REQUEST);
       expect(response.body).toEqual({
         isValid: false,
-        message: 'maxRecords must be a positive integer',
-        code: 'INVALID_START_POSITION',
+        message: 'maxRecords must be a positive integer and at most 10000',
+        code: 'INVALID_MAX_RECORDS',
       });
     });
   });
